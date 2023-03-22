494a93c8053817a0b92cde6f0864a044512c77f9
yoneekee <yoneeki1177@gmail.com>
2023-03-22 12:18:33 +0900

    boot

--- ch03/src/main/java/com/jjang051/ch03/controller/HomeController.java
+++ ch03/src/main/java/com/jjang051/ch03/controller/HomeController.java
@@ -1,14 +1,9 @@
 package com.jjang051.ch03.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * HomeController
- */
 @Controller
-@Slf4j
 public class HomeController {
 
   @GetMapping("/")
