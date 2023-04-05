aca6abe6336253eaac298d8d1bc06b01b2be20a6
yoneekee <yoneeki1177@gmail.com>
2023-04-05 16:58:09 +0900

    jpa

--- jpa/src/main/java/com/jjang051/jpa/service/TodoJpaService.java
+++ jpa/src/main/java/com/jjang051/jpa/service/TodoJpaService.java
@@ -2,7 +2,8 @@ package com.jjang051.jpa.service;
 
 import com.jjang051.jpa.entity.TodoJpa;
 import java.util.List;
-import java.util.Optional;
+
+// import java.util.Optional;
 
 public interface TodoJpaService {
   public void insertTodo(TodoJpa todoJpa);
